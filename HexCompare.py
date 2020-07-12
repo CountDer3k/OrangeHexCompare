28f8f1da4f1faf984d0c039f324fa9cb78316dba
Der3k <42587743+CountDer3k@users.noreply.github.com>
2020-07-12 14:15:29 -0600

    Loading files implemented
    * Can load two files 
    * Can display in hex the first 100 bytes of each file
    * Works under terminal only for now

--- HexCompare.py
+++ HexCompare.py
@@ -1,5 +1,39 @@
 import binascii
-filename = 'test.png'
-with open(filename, 'rb') as f:
-    content = f.read()
-print(binascii.hexlify(content))
+import os
+from tkinter import Tk
+from tkinter.filedialog import askopenfilename
+
+def getHexOf(file):
+	hexString = '    '
+	with open(file, 'rb') as f:
+    # Change 100 to read from the length of entire file
+		for x in range(100):
+			# Reads one byte at a time
+			content = f.read(1)
+			# Converts to hex then to useable string
+			pw_bytes = binascii.hexlify(content)
+			pw_bytes = pw_bytes.decode("utf-8")
+			if((x > 0) and ((x+1) % 4 == 0)):
+				hexString = hexString + str(pw_bytes) + '    ' 
+			else:
+				hexString = hexString + str(pw_bytes) + ' '
+	return hexString
+
+def pickFile():
+	print()
+	Tk().withdraw()
+	file = askopenfilename()
+	fileHex = getHexOf(file)
+	return fileHex
+
+def printFile(num, file):
+	print('File ' + str(num) + ': ')
+	print(file)
+
+
+
+file1Hex = pickFile()
+file2Hex = pickFile()
+printFile(1,file1Hex)
+print('-------------------')
+printFile(2,file2Hex)

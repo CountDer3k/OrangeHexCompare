3236efa1dd18c56db972f7576e6e6d649c89dac8
Count Der3k <der3kburrola@gmail.com>
2020-07-14 17:23:17 -0600

    Fixed not displaying all offsets and information. re-organized layout and alignment

--- HexCompare.py
+++ HexCompare.py
@@ -43,11 +43,14 @@ def pickFile():
 	return getHexOf(file)
 
 def compareFiles(file1, file2):
+	count = 0
 	for i in range(len(file1)):
 		if(file1[i].byteValue != file2[i].byteValue):
+			count = count + 1
 			file1[i].byteColor = "\033[91m"
 			file2[i].byteColor = "\033[91m"
 			print('Byte at: ' + file1[i].byteColor + file1[i].byteOffset + " \033[92m" + 'are different!')
+	return count
 
 def saveToString(data, file, i, ending):
 	hexString = data + file[i].byteColor + file[i].byteValue + "\033[92m" + ending
@@ -60,13 +63,20 @@ def displayBothFilesInHex(file1, file2):
 	for i in range (len(file1)):
 		if(i==0):
 			hexString = str(file1[i].byteOffset) + '\t'
-		if((i+1)%16==0):
+		hexString1 = saveToString(hexString1, file1, i, '')
+		hexString2 = saveToString(hexString2, file2, i, '')
+		if((i+1)%4==0):
+			hexString1= hexString1 + ' '
+			hexString2= hexString2 + ' '
+		if((i+1)%16==0 or i==len(file1)-1):
+			# Check length of hexString1, if it doesn't reach the halfway mark, add filler to make alignment work
+			# Make sure to fix offsets by adding 1 to i, but check that its not added in the last one to prevent out of bounds error
+			
 			hexString = hexString + (hexString1 + ' | ' + hexString2) + '\n' + str(file1[i].byteOffset) + '\t'
 			hexString1 = ''
 			hexString2 = ''
-		else:
-			hexString1 = saveToString(hexString1, file1, i, ' ')
-			hexString2 = saveToString(hexString2, file2, i, ' ')
+		print(str(i))
+			
 	
 	print('File 1: \t\t     |\t\t File 2:')
 	print(hexString)
@@ -80,7 +90,7 @@ def printFile(num, hexString):
 def main():
 	file1Hex = pickFile()
 	file2Hex = pickFile()
-	compareFiles(file1Hex, file2Hex)
+	print('Number of differences: ' + str(compareFiles(file1Hex, file2Hex)))
 	print('\n\n-------------------\n\n')
 	displayBothFilesInHex(file1Hex, file2Hex)
 
